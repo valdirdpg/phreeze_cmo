7b35eaf7a0d151b0b394b2753edf5be52a62ac33
Valdir Nascimento <valdirdpg@hotmail.com>
2018-01-06 20:07:14 -0200

    Removendo todos arquivos que subiram de pasta errada

--- templates/_Footer.tpl.php
+++
@@ -1,9 +0,0 @@
-	<!-- footer -->
-	<div class="container">
-		<hr>
-		<footer>
-			<p class="muted"><small>&copy; <?php echo date('Y'); ?> CONSULTÓRIO MÉDICO E ODONTOLÓGICO - CMO</small></p>
-		</footer>
-	</div>
-	</body>
-</html>
